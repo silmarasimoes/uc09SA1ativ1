94db3e33da7d5910baf80b5b80a9c56d73b8f316
Silmara Simoes <silmara_simoes@yahoo.com.br>
2021-09-28 00:04:01 -0300

    primeiro commit

---
+++ _javascript/alerta.js
@@ -0,0 +1,3 @@
+var botaonegativo;
+botaonegativo=document.getElementById("botaonegativo")
+botaonegativo.addEventListener("click",()=>{alert("Apenas clientes maiores de idade poderão participar das promoções.")},false)
